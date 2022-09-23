886a85a48f16c2624398122c4c7abd9947114e06
dev_suchita <suchitakarokar1997@gmail.com>
2022-09-23 20:22:32 +0530

    sign-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { PasswordRecoveryComponent } from './password-recovery/password-recovery.component';
 import { PasswordSuccessfulComponent } from './password-successful/password-successful.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 const routes: Routes = [
 { path: '',component:ComponentComponent, loadChildren: () => import('./component/component.module').then(m => m.ComponentModule) },
+{path:'sign-up', component:SignUpComponent},
 {path:'login-page',component:LoginPageComponent},
 {path:'password-recovery',component:PasswordRecoveryComponent},
 {path:'password-successful',component:PasswordSuccessfulComponent},
